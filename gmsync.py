f8796c70aa4b720b4b9528817f667feb6249f26f
thebigmunch <munchicus@gmail.com>
2014-04-27 12:53:08 -0400

    Add download functionality to gmsync [Closes #1]

--- gmsync.py
+++ gmsync.py
@@ -6,11 +6,19 @@ More information at https://github.com/thebigmunch/gmusicapi-scripts.
 
 Usage:
   gmsync.py (-h | --help)
+  gmsync.py up [-e PATTERN]... [options] [<input>...]
+  gmsync.py down [-f FILTER]... [options] [<output>]
   gmsync.py [-e PATTERN]... [options] [<input>...]
+
+Commands:
+  up                             Sync local songs to Google Music. Default behavior.
+  down                           Sync Google Music songs to local computer.
 
 Arguments:
   input                          Files, directories, or glob patterns to upload.
                                  Defaults to current directory.
+  output                         Output file or directory name which can include a template pattern.
+                                 Defaults to name suggested by Google Music in your current directory.
 
 Options:
   -h, --help                     Display help message.
@@ -23,6 +31,10 @@ Options:
                                  With -d,--dry-run will display song list.
   -e PATTERN, --exclude PATTERN  Exclude file paths matching a Python regex pattern.
                                  This option can be set multiple times.
+  -f FILTER, --filter FILTER     Filter Google songs by field:pattern pair (e.g. "artist:Muse").
+                                 Songs can match any filter criteria.
+                                 This option can be set multiple times.
+  -a, --all                      Songs must match all filter criteria.
 """
 
 from __future__ import print_function, unicode_literals
@@ -31,9 +43,25 @@ import mutagen
 import os
 import re
 import sys
+import shutil
 
 from gmusicapi import CallFailure
 from gmusicapi.clients import Musicmanager, OAUTH_FILEPATH
+
+INVALID_CHARS = {
+	'\\': '-', '/': ',', ':': '-', '*': 'x', '<': '[',
+	'>': ']', '|': '!', '?': '', '"': "''"
+}
+
+FILTER_FIELDS = [
+	'artist', 'title', 'album', 'album_artist'
+]
+
+TEMPLATE_PATTERNS = {
+	'%artist%': 'artist', '%title%': 'title', '%track%': 'tracknumber',
+	'%track2%': 'tracknumber', '%album%': 'album', '%date%': 'date',
+	'%genre%': 'genre', '%albumartist%': 'albumartist', '%disc%': 'discnumber'
+}
 
 FORMATS = ('.mp3', '.flac', '.ogg', '.m4a')
 
@@ -46,6 +74,56 @@ spaces = re.compile('\s+')
 lead_space = re.compile('^\s+')
 trail_space = re.compile('\s+$')
 the = re.compile('^the\s+', re.I)
+
+
+def clean_tag(tag):
+	"""
+	Clean up metadata tags to improve matching accuracy.
+	"""
+
+	tag = unicode(tag)  # Convert tag to unicode.
+	tag = tag.lower()  # Convert to lower case.
+
+	tag = track_slash.sub('', tag)  # Remove "/<totaltracks>" from track number.
+	tag = lead_zeros.sub(r'\1', tag)  # Remove leading zero(s) from track number.
+	tag = track_dots.sub('', tag)  # Remove dots from track number.
+	tag = non_word.sub('', tag)  # Remove any non-words.
+	tag = spaces.sub(' ', tag)  # Reduce multiple spaces to a single space.
+	tag = lead_space.sub('', tag)  # Remove leading space.
+	tag = trail_space.sub('', tag)  # Remove trailing space.
+	tag = the.sub('', tag)  # Remove leading "the".
+
+	return tag
+
+
+def compare_song_dicts(src_songs, dest_songs):
+	"""
+	Create a list of songs from the difference between source destination song dicts.
+	"""
+
+	transfer_songs = []
+
+	for key, song in src_songs.iteritems():
+		if key not in dest_songs:
+			transfer_songs.append(song)
+
+	return transfer_songs
+
+
+def create_song_key(song):
+	"""
+	Create dict key for song based on metadata.
+	"""
+
+	tags = []
+
+	for tag in filter_tags(song):
+		if isinstance(tag, list):
+			tag = tag[0]
+		tags.append(clean_tag(tag))
+		key = '|'.join(tags)
+
+	return key
 
 
 def do_auth():
@@ -71,24 +149,35 @@ def do_auth():
 	_print("Successfully logged in.\n")
 
 
-def clean_tag(tag):
+def do_download(songs, total):
 	"""
-	Clean up metadata tags to improve matching accuracy.
+	Download songs with a counter.
 	"""
 
-	tag = unicode(tag)  # Convert tag to unicode.
-	tag = tag.lower()  # Convert to lower case.
+	songnum = 0
+	errors = {}
+	pad = len(str(total))
 
-	tag = track_slash.sub('', tag)  # Remove "/<totaltracks>" from track number.
-	tag = lead_zeros.sub(r'\1', tag)  # Remove leading zero(s) from track number.
-	tag = track_dots.sub('', tag)  # Remove dots from track number.
-	tag = non_word.sub('', tag)  # Remove any non-words.
-	tag = spaces.sub(' ', tag)  # Reduce multiple spaces to a single space.
-	tag = lead_space.sub('', tag)  # Remove leading space.
-	tag = trail_space.sub('', tag)  # Remove trailing space.
-	tag = the.sub('', tag)  # Remove leading "the".
+	for song in songs:
+		songnum += 1
 
-	return tag
+		try:
+			_print("Downloading  {0} by {1}".format(song['title'], song['artist']), end="\r")
+			sys.stdout.flush()
+			filename, audio = mm.download_song(song['id'])
+		except CallFailure as e:
+			_print("({num:>{pad}}/{total}) Failed to download  {file} | {error}".format(num=songnum, total=total, file=filename, error=e, pad=pad).encode('utf8'))
+			errors[filename] = e
+		else:
+			filename = make_file_name(filename, audio)
+			shutil.move('temp.mp3', filename)
+			_print("({num:>{pad}}/{total}) Successfully downloaded  {file}".format(num=songnum, total=total, file=filename, pad=pad).encode('utf8'))
+
+	if errors:
+		_print("\n\nThe following errors occurred:\n")
+		for filename, e in errors.iteritems():
+			_print("{file} | {error}".format(file=filename, error=e).encode('utf8'))
+		_print("\nThese files may need to be synced again.\n")
 
 
 def do_upload(files, total):
@@ -158,10 +247,50 @@ def filter_tags(song):
 	return [song[tag] for tag in ['artist', 'album', 'title', 'tracknumber', 'track_number'] if song.get(tag)]
 
 
-def get_file_list():
+def get_google_songs(filters=None, filter_all=False):
 	"""
-	Create a list of supported files from user input(s).
+	Load song list from Google Music library.
 	"""
+
+	_print("Loading Google Music songs...")
+
+	google_songs = {}
+
+	try:
+		songs = mm.get_all_songs()
+	except:
+		songs = mm.get_uploaded_songs()
+
+	if filters:
+		if filter_all:
+			for song in songs:
+				if all(re.search(value, song[field], re.I) for field, value in filters):
+					key = create_song_key(song)
+
+					google_songs[key] = song
+		else:
+			for song in songs:
+				if any(re.search(value, song[field], re.I) for field, value in filters):
+					key = create_song_key(song)
+
+					google_songs[key] = song
+	else:
+		for song in songs:
+			key = create_song_key(song)
+
+			google_songs[key] = song
+
+	_print("Loaded {0} Google Music songs\n".format(len(google_songs)))
+
+	return google_songs
+
+
+def get_local_songs():
+	"""
+	Load song list from local system.
+	"""
+
+	_print("Loading local songs...")
 
 	files = []
 	exclude_files = []
@@ -186,100 +315,140 @@ def get_file_list():
 						else:
 							exclude_files.append(file)
 
-	return files, exclude_files
-
-
-def get_google_songs():
-	"""
-	Load song list from Google Music library.
-	"""
-
-	google_songs = {}
-
-	try:
-		songs = mm.get_all_songs()
-	except:
-		songs = mm.get_uploaded_songs()
-
-	for song in songs:
-		tags = []
-
-		for tag in filter_tags(song):
-			tags.append(clean_tag(tag))
-			key = '|'.join(tags)
-
-		google_songs[key] = song
-
-	return google_songs
-
-
-def get_local_songs(files):
-	"""
-	Load song list from local system.
-	"""
-
 	local_songs = {}
 
 	for file in files:
 		song = mutagen.File(file, easy=True)
 
-		tags = []
-
-		for tag in filter_tags(song):
-			tags.append(clean_tag(tag[0]))
-			key = '|'.join(tags)
+		key = create_song_key(song)
 
 		local_songs[key] = file
 
-	return local_songs
+	_print("Excluded {0} local songs".format(len(exclude_files)))
+	_print("Loaded {0} local songs".format(len(local_songs)))
+
+	return local_songs, exclude_files
+
+
+def make_file_name(filename, audio):
+	"""
+	Create directory structure and file name based on user input.
+	"""
+
+	with open('temp.mp3', 'wb') as temp:
+		temp.write(audio)
+
+	tag = mutagen.File(temp.name, easy=True)
+
+	if cli['output'] != os.getcwd():
+		drive, path = os.path.splitdrive(cli['output'])
+		parts = []
+
+		while True:
+			newpath, tail = os.path.split(path)
+
+			if newpath == path:
+				break
+
+			parts.append(tail)
+			path = newpath
+
+		parts.reverse()
+
+		for i, part in enumerate(parts):
+			for key in TEMPLATE_PATTERNS:
+				if key in part and TEMPLATE_PATTERNS[key] in tag:
+					if key == '%tr2%':
+						tag['tracknumber'] = tag['tracknumber'][0].zfill(2)
+						tag.save()
+
+					parts[i] = parts[i].replace(key, tag[TEMPLATE_PATTERNS[key]][0])
+
+			for char in INVALID_CHARS:
+				if char in parts[i]:
+					parts[i] = parts[i].replace(char, INVALID_CHARS[char])
+
+		filename = os.path.join(drive, *parts) + '.mp3'
+
+		basename, __ = os.path.split(filename)
+
+		if basename:
+			try:
+				os.makedirs(basename)
+			except OSError:
+				if not os.path.isdir(basename):
+					raise
+
+	return filename
 
 
 def main():
 	do_auth()
 
-	_print("Loading local songs...")
-	files, exclude_files = get_file_list()
-	_print("Excluded {0} local songs".format(len(exclude_files)))
-	local_songs = get_local_songs(files)
-	_print("Loaded {0} local songs\n".format(len(local_songs)))
-
-	_print("Loading Google Music songs...")
-	google_songs = get_google_songs()
-	_print("Loaded {0} Google Music songs\n".format(len(google_songs)))
-
-	_print("Scanning for missing songs...")
-
-	upload_files = []
-
-	# Compare local and Google Music library lists.
-	for key, song in local_songs.iteritems():
-		if key not in google_songs:
-			upload_files.append(song)
-
-	# Sort lists for sensible output.
-	upload_files.sort()
-	exclude_files.sort()
-
-	total = len(upload_files)
-
-	if cli['dry-run']:
-		_print("Found {0} songs".format(total))
-
-		if upload_files:
-			_print("\nSongs to upload:\n")
-			for file in upload_files:
-				print(file.encode('utf8'))
-
-		if exclude_files:
-			_print("\nSongs to exclude:\n")
-			for file in exclude_files:
-				print(file.encode('utf8'))
+	if cli['filter']:
+		filters = [
+			tuple(filter.split(':', 1)) for filter in cli['filter'] if filter.split(':', 1)[0] in FILTER_FIELDS
+		]
 	else:
-		if upload_files:
-			_print("Uploading {0} songs to Google Music\n".format(total))
-			do_upload(upload_files, total)
+		filters = None
+
+	google_songs = get_google_songs(filters, cli['all'])
+	local_songs, exclude_songs = get_local_songs()
+
+	if cli['down']:
+		_print("\nScanning for missing songs...")
+		download_songs = compare_song_dicts(google_songs, local_songs)
+
+		# Sort the list for sensible output.
+		download_songs.sort(key=lambda song: (song['artist'], song['album'], song['track_number']))
+		total = len(download_songs)
+
+		if cli['dry-run']:
+			_print("Found {0} songs to download\n".format(total))
+
+			if download_songs:
+				_print("Songs to download:\n")
+				for song in download_songs:
+					print("{0} by {1}".format(song['title'], song['artist']).encode('utf8'))
+			else:
+				_print("No songs to download")
 		else:
-			_print("No songs to upload")
+			if download_songs:
+				_print("Downloading {0} songs from Google Music\n".format(total))
+				do_download(download_songs, total)
+			else:
+				_print("No songs to download")
+	else:
+		_print("\nScanning for missing songs...")
+
+		upload_files = compare_song_dicts(local_songs, google_songs)
+
+		# Sort lists for sensible output.
+		upload_files.sort()
+		exclude_songs.sort()
+
+		total = len(upload_files)
+
+		if cli['dry-run']:
+			_print("Found {0} songs to upload\n".format(total))
+
+			if upload_files:
+				_print("Songs to upload:\n")
+				for file in upload_files:
+					print(file.encode('utf8'))
+			else:
+				_print("No songs to upload")
+
+			if exclude_songs:
+				_print("\nSongs to exclude:\n")
+				for file in exclude_songs:
+					print(file.encode('utf8'))
+		else:
+			if upload_files:
+				_print("Uploading {0} songs to Google Music\n".format(total))
+				do_upload(upload_files, total)
+			else:
+				_print("No songs to upload")
 
 	# Log out mm session when finished.
 	mm.logout()
