2d3da9164da2cf1d98853cfd474beb555585853f
thebigmunch <munchicus@gmail.com>
2014-08-18 13:15:22 -0400

    Improve safe_print function

--- utils.py
+++ utils.py
@@ -2,11 +2,13 @@
 
 from __future__ import print_function, unicode_literals
 
+import sys
+
 
 def safe_print(msg, *args, **kwargs):
 	"""Safely print strings containing unicode characters."""
 
 	try:
 		print(msg, *args, **kwargs)
-	except UnicodeEncodeError:
-		print(msg.encode('utf8'), *args, **kwargs)
+	except:
+		print(msg.encode(sys.stdout.encoding or 'utf8', errors='replace'), *args, **kwargs)
