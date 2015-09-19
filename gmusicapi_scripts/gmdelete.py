07e56bd19e01951bce2c9fa3bf4df8fd0eb95f3d
thebigmunch <mail@thebigmunch.me>
2015-09-19 14:06:13 -0400

    Add better workaround for unicode characters in path/file names on Windows
    Note: There is still an issue in Python 2's subprocess module that wil fail to properly send
    path/file names with unicode characters to avconv/ffmpeg that is out of my control. Any file
    with unicode characters in them that require transcoding will fail on the transcoding step
    with a message saying as such. This includes non-mp3 files as well as mp3 files when
    scan-and-match is enabled.

--- gmusicapi_scripts/gmdelete.py
+++ gmusicapi_scripts/gmdelete.py
@@ -35,6 +35,7 @@ Patterns can be any valid Python regex patterns.
 from __future__ import unicode_literals
 
 import logging
+import os
 import sys
 
 from docopt import docopt
@@ -48,11 +49,37 @@ logger = logging.getLogger('gmusicapi_wrapper')
 sh = logging.StreamHandler()
 logger.addHandler(sh)
 
-encoding = sys.getfilesystemencoding()
+
+# From https://code.activestate.com/recipes/572200/
+def win32_unicode_argv():
+	"""Uses shell32.GetCommandLineArgvW to get sys.argv as a list of Unicode strings."""
+
+	from ctypes import POINTER, byref, cdll, c_int, windll
+	from ctypes.wintypes import LPCWSTR, LPWSTR
+
+	GetCommandLineW = cdll.kernel32.GetCommandLineW
+	GetCommandLineW.argtypes = []
+	GetCommandLineW.restype = LPCWSTR
+
+	CommandLineToArgvW = windll.shell32.CommandLineToArgvW
+	CommandLineToArgvW.argtypes = [LPCWSTR, POINTER(c_int)]
+	CommandLineToArgvW.restype = POINTER(LPWSTR)
+
+	cmd = GetCommandLineW()
+	argc = c_int(0)
+	argv = CommandLineToArgvW(cmd, byref(argc))
+	if argc.value > 0:
+		# Remove Python executable and commands if present
+		start = argc.value - len(sys.argv)
+		return [argv[i] for i in xrange(start, argc.value)]
 
 
 def main():
-	sys.argv = [arg if isinstance(arg, unicode) else arg.decode(encoding) for arg in sys.argv]
+	if os.name == 'nt':
+		sys.argv = win32_unicode_argv()
+	else:
+		sys.argv = [arg.decode(sys.stdin.encoding) for arg in sys.argv]
+
 	cli = dict((key.lstrip("-<").rstrip(">"), value) for key, value in docopt(__doc__).items())
 
 	if cli['quiet']:
