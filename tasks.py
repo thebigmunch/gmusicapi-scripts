1766736dffe4b84c161fccbfe2fdf037585e9380
thebigmunch <mail@thebigmunch.me>
2016-02-15 08:08:47 -0500

    Add tasks file for use with Invoke

---
+++ tasks.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+
+"""Useful task commands for development and maintenance."""
+
+from invoke import run, task
+
+
+@task
+def clean():
+	"""Clean the project directory of unwanted files and directories."""
+
+	run('rm -rf gmusicapi_scripts.egg-info')
+	run('rm -rf .coverage')
+	run('rm -rf .tox')
+	run('rm -rf .cache')
+	run('rm -rf build/')
+	run('rm -rf dist/')
+	run('find . -name *.pyc -delete')
+	run('find . -name *.pyo -delete')
+	run('find . -name __pycache__ -delete -depth')
+	run('find . -name *~ -delete')
+
+
+@task(clean)
+def build():
+	"""Build sdist and bdist_wheel distributions."""
+
+	run('python setup.py sdist bdist_wheel')
+
+
+@task(build)
+def deploy():
+	"""Build and upload gmusicapi_scripts distributions."""
+
+	upload()
+
+
+@task
+def upload():
+	"""Upload gmusicapi_scripts distributions using twine."""
+
+	run('twine upload dist/*')
