b45effe158ed3bdf5749daeb90d483d488500b77
thebigmunch <mail@thebigmunch.me>
2016-06-03 19:28:01 -0400

    Update tasks

--- tasks.py
+++ tasks.py
@@ -2,24 +2,33 @@
 
 """Useful task commands for development and maintenance."""
 
+import glob
+import os
+import shutil
+
 from invoke import run, task
+
+to_remove_dirnames = ['**/__pycache__', '.cache', '.tox', 'build', 'dist', 'gmusicapi_scripts.egg-info']
+to_remove_filenames = ['**/*.pyc', '**/*.pyo', '.coverage']
 
 
 @task
 def clean():
 	"""Clean the project directory of unwanted files and directories."""
 
-	run('rm -rf gmusicapi_scripts.egg-info')
-	run('rm -rf .coverage')
-	run('rm -rf .tox')
-	run('rm -rf .cache')
-	run('rm -rf build/')
-	run('rm -rf dist/')
-	run('rm -rf site/')
-	run('find . -name *.pyc -delete')
-	run('find . -name *.pyo -delete')
-	run('find . -name __pycache__ -delete -depth')
-	run('find . -name *~ -delete')
+	to_remove_dirs = [
+		path for dirname in to_remove_dirnames for path in glob.glob(dirname) if os.path.isdir(path)
+	]
+
+	for dirpath in to_remove_dirs:
+		shutil.rmtree(dirpath)
+
+	to_remove_files = [
+		path for filename in to_remove_filenames for path in glob.glob(filename) if os.path.isfile(path)
+	]
+
+	for filepath in to_remove_files:
+		os.remove(filepath)
 
 
 @task(clean)
@@ -30,10 +39,17 @@ def build():
 
 
 @task(build)
-def deploy():
+def publish():
 	"""Build and upload gmusicapi_scripts distributions."""
 
 	upload()
+
+
+@task
+def upload():
+	"""Upload gmusicapi_scripts distributions using twine."""
+
+	run('twine upload dist/*')
 
 
 @task
@@ -44,10 +60,3 @@ def docs(test=False):
 		run('mkdocs serve')
 	else:
 		run('mkdocs gh-deploy --clean')
-
-
-@task
-def upload():
-	"""Upload gmusicapi_scripts distributions using twine."""
-
-	run('twine upload dist/*')
